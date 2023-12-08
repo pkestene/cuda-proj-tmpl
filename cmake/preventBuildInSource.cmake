90cccde59d75fc40f6b9dddb6d9b327e2fd995fb
Pierre Kestener <pierre.kestener@cea.fr>
2023-12-08 15:21:55 +0100

    Update cmake macro.

--- cmake/preventBuildInSource.cmake
+++ cmake/preventBuildInSource.cmake
@@ -1,3 +1,19 @@
-if(${CMAKE_SOURCE_DIR} STREQUAL ${CMAKE_BINARY_DIR})
-  message(FATAL_ERROR "In-source builds not allowed. Please make a new directory (called a build directory) and run CMake from there. You may need to remove CMakeCache.txt. ")
-endif()
+function(prevent_build_in_source)
+
+  # make sure the user doesn't play dirty with symlinks
+  get_filename_component(srcdir "${CMAKE_SOURCE_DIR}" REALPATH)
+  get_filename_component(bindir "${CMAKE_BINARY_DIR}" REALPATH)
+
+  # disallow in-source builds
+  if("${srcdir}" STREQUAL "${bindir}")
+    message("######################################################")
+    message("# ${PROJECT_NAME} should not be configured and built in the source directory")
+    message("# You must run cmake from a build directory.")
+    message("# For example:")
+    message("# mkdir _build ; cd _build")
+    message("# run cmake from the build directory.")
+    message("######################################################")
+    message(FATAL_ERROR "Quitting configuration")
+  endif()
+
+endfunction(prevent_build_in_source)
